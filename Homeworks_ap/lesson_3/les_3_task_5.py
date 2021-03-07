cdd2b4da3d66bc60544638850fe1ec4415ef4ae3
mstislav <strong13strong@gmail.com>
2021-03-07 18:12:08 +0700

    n_f commit

--- Homeworks_ap/lesson_3/les_3_task_5.py
+++ Homeworks_ap/lesson_3/les_3_task_5.py
@@ -1,5 +1,20 @@
 '''
-
+Матрица 5x4 заполняется вводом с клавиатуры, кроме последних элементов строк.
+Программа должна вычислять сумму введенных элементов каждой строки и
+записывать ее в последнюю ячейку строки. В конце следует вывести полученную матрицу.
 '''
 
+new_matrix = []
 
+for _ in range(4):
+    new_matrix.append([])
+    sum = 0
+    for num_ in range(4):
+        user_number = int(input(f'Введите {_+1} элемент {num_+1} столбца: '))
+        sum += user_number
+        new_matrix[_].append(user_number)
+
+    new_matrix[_].append(sum)
+
+for n in new_matrix:
+    print(('{:>4d}' * 5).format(*n))
