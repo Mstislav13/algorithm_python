3db136252eadd81e0b5756e66c2a112701bfb4eb
Mstislav13 <71246531+Mstislav13@users.noreply.github.com>
2021-03-10 01:34:55 +0700

    Update les_3_task_5.py

--- Homeworks_ap/lesson_3/les_3_task_5.py
+++ Homeworks_ap/lesson_3/les_3_task_5.py
@@ -6,15 +6,15 @@
 
 new_matrix = []
 
-for _ in range(4):
+for i in range(4):
     new_matrix.append([])
     sum = 0
     for num_ in range(4):
-        user_number = int(input(f'Введите {_+1} элемент {num_+1} столбца: '))
+        user_number = int(input(f'Введите {i+1} элемент {num_+1} столбца: '))
         sum += user_number
-        new_matrix[_].append(user_number)
+        new_matrix[i].append(user_number)
 
-    new_matrix[_].append(sum)
+    new_matrix[i].append(sum)
 
 for n in new_matrix:
     print(('{:>4d}' * 5).format(*n))
